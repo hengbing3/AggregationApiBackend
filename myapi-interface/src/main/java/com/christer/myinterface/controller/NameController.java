0aacd3c2837033b092744ecd507f1d7a6552130f
Christer <hongweibin3@gmail.com>
2024-03-07 21:55:43 +0800

    feat: 接口校验去除，改为网关拦截校验

--- myapi-interface/src/main/java/com/christer/myinterface/controller/NameController.java
+++ myapi-interface/src/main/java/com/christer/myinterface/controller/NameController.java
@@ -8,6 +8,7 @@ import javax.servlet.http.HttpServletRequest;
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.ThreadPoolExecutor;
 
 /**
  * @author Christer
@@ -20,7 +21,11 @@ import java.nio.charset.StandardCharsets;
 public class NameController {
 
     @GetMapping("/")
-    public String getNameByGet(String name) {
+    public String getNameByGet(String name, HttpServletRequest request) {
+        System.out.println("请求头header: " + request.getHeader("christer"));
+        if (name.equals("aa")) {
+            throw new RuntimeException("请重试！" + System.currentTimeMillis());
+        }
         return "GET 你的名字是:" + name;
     }
 
@@ -33,32 +38,32 @@ public class NameController {
     public String getUsernameByPost(@RequestBody User user, HttpServletRequest request) {
         // 1.拿到这五个我们可以一步一步去做校验,比如 accessKey 我们先去数据库中查一下
         // 从请求头中获取参数
-        String accessKey = request.getHeader("accessKey");
-        String nonce = request.getHeader("nonce");
-        String timestamp = request.getHeader("timestamp");
-        String sign = request.getHeader("sign");
-        String body = null;
-        try {
-            body = URLDecoder.decode(request.getHeader("body"), "UTF-8");
-        } catch (UnsupportedEncodingException e) {
-            throw new RuntimeException(e);
-        }
-
-        if (!accessKey.equals("abin") ) {
-            throw new RuntimeException("无权限!");
-        }
-        // 随机数可以存储到Redis或者HashMap 校验是否被使用过，可以过5min清空一次数据,用于防止重放
-        if (nonce.length() != 6) {
-            throw new RuntimeException("无权限");
-        }
-        // 只对一分钟以内的请求有效 随机数和时间戳可以配合，防止重放
-        if ((System.currentTimeMillis() / 1000) - Long.parseLong(timestamp) > 1 * 60) {
-            throw new RuntimeException("无权限");
-        }
-        String genSign = SignUtils.genSign(body, "abcdefg");
-        if (!sign.equals(genSign)) {
-            throw new RuntimeException("无权限");
-        }
+//        String accessKey = request.getHeader("accessKey");
+//        String nonce = request.getHeader("nonce");
+//        String timestamp = request.getHeader("timestamp");
+//        String sign = request.getHeader("sign");
+//        String body = null;
+//        try {
+//            body = URLDecoder.decode(request.getHeader("body"), "UTF-8");
+//        } catch (UnsupportedEncodingException e) {
+//            throw new RuntimeException(e);
+//        }
+//
+//        if (!accessKey.equals("abin") ) {
+//            throw new RuntimeException("无权限!");
+//        }
+//        // 随机数可以存储到Redis或者HashMap 校验是否被使用过，可以过5min清空一次数据,用于防止重放
+//        if (nonce.length() != 6) {
+//            throw new RuntimeException("无权限");
+//        }
+//        // 只对一分钟以内的请求有效 随机数和时间戳可以配合，防止重放
+//        if ((System.currentTimeMillis() / 1000) - Long.parseLong(timestamp) > 1 * 60) {
+//            throw new RuntimeException("无权限");
+//        }
+//        String genSign = SignUtils.genSign(body, "abcdefg");
+//        if (!sign.equals(genSign)) {
+//            throw new RuntimeException("无权限");
+//        }
         //
         return "POST 你的用户名是:" + user.getUsername();
     }
--- myapi-interface/src/main/java/com/christer/myinterface/utils/SignUtils.java
+++ myapi-interface/src/main/java/com/christer/myinterface/utils/SignUtils.java
@@ -1,10 +1,7 @@
 package com.christer.myinterface.utils;
 
 import cn.hutool.crypto.digest.DigestAlgorithm;
-import cn.hutool.crypto.digest.DigestUtil;
 import cn.hutool.crypto.digest.Digester;
-
-import java.util.Map;
 
 /**
  * @author Christer
@@ -16,7 +13,7 @@ public class SignUtils {
     /**
      * 生成签名认证算法
      *
-     * @param body   包含需要签名的参数的哈希映射
+     * @param body      包含需要签名的参数的哈希映射
      * @param secretKey 密钥
      * @return 生成的签名字符串
      */
@@ -24,7 +21,7 @@ public class SignUtils {
         // 使用SHA256算法的Digester
         final Digester digester = new Digester(DigestAlgorithm.SHA256);
         // 构建签名内容，将哈希映射转换为字符串并拼接密钥
-        final String content = body + secretKey;
+        final String content = body + ":" + secretKey;
         return digester.digestHex(content);
     }
 }
